98ce9b474a6db2b8c24f30fa759186ad786bd9be
Mitali Shah <mitalishaleenshah@gmail.com>
2023-07-06 14:32:50 -0500

    added css reset and some basic structure for header

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+
+export default function Header() {
+    return (
+        <>
+            <h1>Super Sticky Note</h1>
+            <aside>
+                <button>+ New Note</button>
+                <input
+                    type="text"
+                    placeholder="Type here to search..."
+                />
+            </aside>
+        </>
+        
+    )
+}
